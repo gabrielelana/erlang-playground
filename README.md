26061c4e7aa168f52ee7375726af106d24f61b07
gabriele.lana <gabriele.lana@cleancode.it>
2014-07-18 17:19:25 +0200

    8 Queens Problem

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-erlang-playground
-=================
-
+## Erlang Playground
 Some problems and kata solved in erlang to practice with the language
+
+* `queens.erl` - [8 Queens Problem](http://en.wikipedia.org/wiki/Eight_queens_puzzle) generalized for N queens on a chess board of size N.
+
---
+++ queens.erl
@@ -0,0 +1,20 @@
+-module(queens).
+-export([queens/1]).
+
+queens(N) -> queens(N, lists:seq(1,N,1)).
+
+queens(0, _AllColumnPositions) -> [[]];
+queens(N, AllColumnPositions) -> [
+  [Column|ColumnsAlreadyTaken] ||
+    ColumnsAlreadyTaken <- queens(N-1, AllColumnPositions),
+    Column <- AllColumnPositions -- ColumnsAlreadyTaken,
+    safe(Column, ColumnsAlreadyTaken)
+].
+
+safe(Column, ColumnsAlreadyTaken) -> safe(Column, ColumnsAlreadyTaken, 1).
+
+safe(_Column, [], _Distance) -> true;
+safe(Column, [ColumnTaken|RestOfColumnsTaken], DistanceToColumnTaken) ->
+  (Column /= ColumnTaken + DistanceToColumnTaken) and
+  (Column /= ColumnTaken - DistanceToColumnTaken) and
+  safe(Column, RestOfColumnsTaken, DistanceToColumnTaken + 1).
